c45f0af85e965ff7bb93e5bbd7673565d73b13d3
Kateryna-Urbanovych <katya_urb@ukr.net>
2021-09-08 17:04:26 +0300

    fix

--- src/servises/openWeather-api.js
+++ src/servises/openWeather-api.js
@@ -1,10 +1,8 @@
 const BASE_URL_WEATHER = 'https://api.openweathermap.org/data/2.5';
-const KEY_WEATHER = 'b6b6aebead40b953adf28b351d9de3d0';
-// const KEY_WEATHER = process.env.REACT_APP_KEY_WEATHER;
+const KEY_WEATHER = process.env.REACT_APP_KEY_WEATHER;
 
 const BASE_URL_GEOCODING = 'https://www.mapquestapi.com/geocoding';
-const KEY_GEOCODING = 'qSuR24jOiYXFslVnUT1m16v3WTEnFtGC';
-// const KEY_GEOCODING = process.env.REACT_APP_KEY_GEOCODING;
+const KEY_GEOCODING = process.env.REACT_APP_KEY_GEOCODING;
 
 async function fetchWithErrorHandling(url = '') {
     const response = await fetch(url);
